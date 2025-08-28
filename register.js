59d62e5894102f9a93432a603be41b150c3d661c
Lincoln Madaraka <117019629+Lincoln-Madaraka@users.noreply.github.com>
2025-08-28 05:14:00 +0300

    tutorial

--- register.js
+++ register.js
@@ -1,11 +1,16 @@
 document.getElementById("registerForm").addEventListener("submit", function(event){
   event.preventDefault();
+  let email = document.getElementById("email").value;
+
+  if(email === ""){
+    alert("Please enter your email!");
+    return;
+  }
 
   let password = document.getElementById("password").value;
-  let confirmPassword = document.getElementById("confirmPassword").value;
 
-  if(password !== confirmPassword){
-    alert("Passwords do not match!");
+  if(password !== ""){
+    alert("Please Enter a password!");
     return;
   }
 
